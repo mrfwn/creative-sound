97fd4f77ca94e653aee3d75c304c2f1079863d8d
Arthur Pires <arthur.limapires@gmail.com>
2021-05-02 18:49:52 -0300

    Interpretating correctly the piano's input from mobile

--- frontend/src/components/FileList/index.tsx
+++ frontend/src/components/FileList/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 import { MdCheckCircle, MdError, MdMoodBad, MdKeyboardVoice } from "react-icons/md";
 import { FaMusic } from "react-icons/fa";
 import { GiMusicalKeyboard, GiGuitar, GiDrum } from "react-icons/gi";
@@ -10,49 +10,101 @@ import socketIOClient from "socket.io-client";
 
 
 import { Container, FileInfo, Preview } from "./styles";
+import { Dictionary } from "lodash";
 
 
 const FileList = () => {
   const { uploadedFiles: files, deleteFile } = useFiles();
-  const [response, setResponse] = useState("");
+  const [response, setResponse] = useState<any>({});
   const [audioSrc, setAudioSrc] = useState("");
+  var lastPackage = "";
+  const [sounds, setSounds] = useState<Dictionary<Howl>>({});
+  const [soundIds, setSoundIds] = useState<Dictionary<number>>({});
+  const [soundLoop, setSoundLoop] = useState<boolean>(false);
 
   const getProperCss = (src: string) => {
     const css = {cursor:"pointer", borderStyle: "none", width: 32, height: 32};
-    if (src == audioSrc) {
+    if (src === audioSrc) {
       css.borderStyle = "dotted";
     }
     return css
   }
 
-  useEffect(() => {
-    if (files.length > 0) {
-      const audio = audioSrc;
-      var sound = new Howl({ // TODO: Change the way sound is setted
-        src: [audio],
-        sprite: {
-          blast: [0, 3000],
-          laser: [4000, 1000],
-          winner: [6000, 5000]
-        }
-      });
-      sound.play('winner');
-    }
+  const getHowl = (audio: string): Howl => {
+    return new Howl({
+      src: [audioSrc],
+      sprite: {
+        push1false:    [0   , 1000],
+        push2false:    [1000, 1000],
+        push3false:    [2000, 1000],
+        push4false:    [3000, 1000],
+        push5false:    [4000, 1000],
+        push6false:    [5000, 1000],
+        push7false:    [6000, 1000],
+        push8false:    [7000, 1000],
+        push9false:    [8000, 1000],
+        push10false:   [9000, 1000],
+        push1true:     [0   , 1000, true],
+        push2true:     [1000, 1000, true],
+        push3true:     [2000, 1000, true],
+        push4true:     [3000, 1000, true],
+        push5true:     [4000, 1000, true],
+        push6true:     [5000, 1000, true],
+        push7true:     [6000, 1000, true],
+        push8true:     [7000, 1000, true],
+        push9true:     [8000, 1000, true],
+        push10true:    [9000, 1000, true],
+      }
+    })
+  };
 
+  useEffect(() => {
+    if (audioSrc === "")
+      return;
+    
+    const address = (response && response.address) || "";
+    const value = (response && response.args && response.args[0] && response.args[0].value) || 0;
+
+    if (address.includes("push")) {
+      if (value !== 0) {
+        const sprite = address.split('/')[2] + soundLoop;
+        const soundId = audioSrc + sprite + soundLoop;
+        var sound = sounds[audioSrc]
+        if (sound === undefined) {
+          sound = getHowl(audioSrc);
+          var newSounds = sounds;
+          newSounds[audioSrc] = sound;
+          setSounds(newSounds);
+        }
+
+        const newSoundIds = soundIds;
+        if (soundLoop) {
+          if (soundIds[soundId]) {
+            sound.stop(soundIds[soundId]);
+            delete newSoundIds[soundId];
+          } else {
+            newSoundIds[soundId] = sound.play(sprite);
+          }
+        } else {
+          newSoundIds[soundId] = sound.stop(soundIds[soundId]).play(soundIds[soundId] || sprite);
+        }
+        setSoundIds(newSoundIds);
+      }
+    } else if (address.includes("toggle")) {
+      setSoundLoop(value === 1);
+    }
   }, [response]);
 
   useEffect(() => {
     const socket = socketIOClient("http://localhost:3333");
     socket.on("FromAPI", data => {
-      setResponse(JSON.stringify(data));
+      if (lastPackage === JSON.stringify(data))
+        return;
+        lastPackage = JSON.stringify(data)
+      
+      setResponse(data);
     });
   }, []);
-
-  useEffect(() => {
-    const data = {"address":"/3/push1","args":[{"type":"f","value":0}]};
-    setResponse(JSON.stringify(data));
-    console.log(data)
-  }, [audioSrc]);
 
   if (!files.length)
     return (
